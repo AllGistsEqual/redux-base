f029e24ef0b680d95a6270885b0074241098bd47
Konrad Abe <mail@allbitsequal.com>
2019-09-22 17:28:15 +0200

    fixed linting pre tutorial code

--- src/components/CopyClicker.jsx
+++ src/components/CopyClicker.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const CopyClicker = (props) => {
     const { initialCopies, initialCpC } = props
-    const [ copies, setCopies ] = useState(initialCopies)
-    const [ CpC, setCpC ] = useState(initialCpC)
+    const [copies, setCopies] = useState(initialCopies)
+    const [CpC, setCpC] = useState(initialCpC)
 
     const onCopyClicked = () => {
         setCopies(copies + CpC)
@@ -12,7 +12,7 @@ const CopyClicker = (props) => {
 
     const onImproveCopy = () => {
         const cost = CpC * CpC + 1
-        if ( copies >= cost) {
+        if (copies >= cost) {
             setCopies(copies - cost)
             setCpC(CpC + 1)
         }
@@ -20,19 +20,25 @@ const CopyClicker = (props) => {
 
     return (
         <div>
-            <button className="theCopy" onClick={onCopyClicked}>
+            <button type="button" className="theCopy" onClick={onCopyClicked}>
                 <span role="img" aria-label="ClickThatCopy">💾</span>
             </button>
 
             <h2>CLICK THE COPY!</h2>
 
-            <p><span role="img" aria-label="copies">💾: {copies} copies</span></p>
+            <p>
+                <span role="img" aria-label="copies">
+                    {`💾: ${copies} copies`}
+                </span>
+            </p>
 
-            <button onClick={onImproveCopy}>
-                Upgrade ClicksPerCopy for {CpC * CpC + 1} copies
+            <button type="button" onClick={onImproveCopy}>
+                {`Upgrade ClicksPerCopy for ${CpC * CpC + 1} copies`}
             </button>
 
-            <p title="copies per click">CpC: {CpC}</p>
+            <p title="copies per click">
+                {`CpC: ${CpC}`}
+            </p>
         </div>
     )
 }
