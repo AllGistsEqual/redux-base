038dd9b11a7f6d6cf5b3eead12c617b973d6bb43
Konrad Abe <mail@allbitsequal.com>
2019-09-22 22:39:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,15 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     console.log('reducer action: ', action)
-    return state
+    const { type } = action
+    switch (type) {
+        case 'COPY_CLICK':
+            return {
+                copies: state.copies + 1,
+            }
+        default:
+            return state
+    }
 }
 
 const store = createStore(reducer)
@@ -17,6 +25,8 @@ const store = createStore(reducer)
 console.log('Our current state: ', store.getState())
 
 store.dispatch({ type: 'COPY_CLICK' })
+
+console.log('Our new state: ', store.getState())
 
 function App() {
     return (
